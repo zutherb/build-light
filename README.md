16376929b4986a8d7fc400974b1629d3f5c2aafa
Bernd Zuther <bernd.zuther@comsysto.com>
2013-08-12 22:16:44 +0200

    add documentation

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 Build Light
 ===========
 
-What is build light
+What is Build Light
 -------------------
 
 ![Build Light](picture/Build-Light.jpg)
+
+Cleware Traffic Java Driver
+---------------------------
+
+[Download](https://raw.github.com/zutherb/build-light/master/bin/cleware.jar)
+
+```bash
+java -jar cleware.jar
+java -jar cleware-0.1-DEV.jar red on wait 500 yellow on wait 500 green on
+java -jar cleware-0.1-DEV.jar red off wait 500 yellow off wait 500 green off
+```
--- driver/cleware/src/main/java/com/cleware/commandline/CommandLineRunner.java
+++ driver/cleware/src/main/java/com/cleware/commandline/CommandLineRunner.java
@@ -6,8 +6,6 @@ import com.cleware.commandline.command.LedCommand;
 import com.cleware.commandline.command.WaitCommand;
 import com.cleware.driver.TrafficLight;
 import com.cleware.driver.TrafficLightFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,12 +18,8 @@ public final class CommandLineRunner {
 
     private CommandLineRunner() { /*NOOP*/ }
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CommandLineRunner.class);
     private static final TrafficLight TRAFFIC_LIGHT = TrafficLightFactory.instance();
     private static final List<Command> COMMAND_CHAIN = Arrays.asList(new GuiCommand(), new LedCommand(), new WaitCommand());
-
-    private static final String ON = "on";
-    private static final String OFF = "off";
 
     public static void main(String[] args) {
         try {
